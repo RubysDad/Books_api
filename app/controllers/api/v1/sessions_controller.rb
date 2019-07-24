57846a4d3aa3a7c325bcacc51b3c37b859eaee5b
Rubysdad <marktravismorales@gmail.com>
2019-07-24 14:53:50 -0500

    Implemented sign out endpoint

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,6 +3,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   # sign in
   def create
@@ -12,6 +14,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response('Unauthorized', false, {}, :unauthorized)
     end
+  end
+
+  # log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response('Log out Successfully', true, {}, :ok)
   end
 
   private
@@ -26,5 +35,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     return @user if @user.present?
 
     json_response("Cannot find User", false, {}, :failure)
+  end
+
+  def valid_token
+    @user = User.find_by(authentication_token: request.headers["AUTH-TOKEN"])
+
+    return @user if @user.present?
+
+    json_response('Invalid Token', false, {}, :failure)
   end
 end
